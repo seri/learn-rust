462a69a167c48b632b80ae4a62b66d1d40970ac7
seri <seritrinh@gmail.com>
2026-05-02 22:55:08 +0700

    implement iterator for binary tree

--- binary-tree/src/main.rs
+++ binary-tree/src/main.rs
@@ -44,12 +44,63 @@ impl<T: PartialOrd> BinaryTree<T> {
     }
 }
 
+struct InorderIter<'a, T> {
+    stack: Vec<&'a BinaryTree<T>>,
+}
+
+impl<'a, T> IntoIterator for &'a BinaryTree<T> {
+    type Item = &'a T;
+    type IntoIter = InorderIter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        InorderIter::new(self)
+    }
+}
+
+impl<'a, T> InorderIter<'a, T> {
+    fn new(tree: &'a BinaryTree<T>) -> Self {
+        let mut iter = Self { stack: Vec::new() };
+        iter.push_left(tree);
+        iter
+    }
+
+    fn push_left(&mut self, mut tree: &'a BinaryTree<T>) {
+        while let BinaryTree::Node {
+            value: _,
+            left,
+            right: _,
+        } = tree
+        {
+            self.stack.push(tree);
+            tree = left;
+        }
+    }
+}
+
+impl<'a, T> Iterator for InorderIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.stack.pop() {
+            Some(BinaryTree::Node { value, right, .. }) => {
+                self.push_left(right);
+                Some(value)
+            }
+            _ => None,
+        }
+    }
+}
+
 fn main() {
     let mut tree: BinaryTree<isize> = BinaryTree::Empty;
     tree.add(2);
     tree.add(1);
     tree.add(3);
     println!("{:?}", tree);
+
+    for elem in &tree {
+        println!("{:}", elem);
+    }
 
     let mut tree2: BinaryTree<&str> = BinaryTree::Empty;
     tree2.add("bbb");
@@ -58,4 +109,8 @@ fn main() {
     println!("{:?}", tree2);
     println!("{:?}", tree2.contains("ddd"));
     println!("{:?}", tree2.contains("aaa"));
+
+    for elem in &tree2 {
+        println!("{:}", elem);
+    }
 }
