21e88cf6c74500c2ae7ff62b8a7f1cfbd1ea1056
seri <seritrinh@gmail.com>
2026-09-18 16:26:43 +0700

    complete the bencode parser

--- bittorrent-client/src/main.rs
+++ bittorrent-client/src/main.rs
@@ -23,7 +23,7 @@ impl Bencode {
         Ok((Bencode::Integer(value), &bytes[pos + 1..]))
     }
 
-    fn parse_string(bytes: &[u8]) -> Result<(Self, &[u8]), String> {
+    fn parse_string_raw(bytes: &[u8]) -> Result<(Vec<u8>, &[u8]), String> {
         let start = bytes
             .iter()
             .position(|&b| b == b':')
@@ -41,32 +41,57 @@ impl Bencode {
                 bytes.len()
             ));
         }
-        Ok((
-            Bencode::String(Vec::from(&bytes[start + 1..end])),
-            &bytes[end..],
-        ))
+        Ok((Vec::from(&bytes[start + 1..end]), &bytes[end..]))
+    }
+
+    fn parse_string(bytes: &[u8]) -> Result<(Self, &[u8]), String> {
+        let (raw_string, remainder) = Self::parse_string_raw(bytes)?;
+        Ok((Bencode::String(raw_string), remainder))
     }
 
     fn parse_list(bytes: &[u8]) -> Result<(Self, &[u8]), String> {
-        let mut value = Vec::<Bencode>::new();
+        let mut list = Vec::<Bencode>::new();
         let mut remainder = &bytes[1..];
+
         while remainder.len() > 0 {
-            println!("remainder: {}", String::from_utf8_lossy(remainder));
             if remainder[0] == b'e' {
-                break
+                break;
             }
-            let elem_wrapper = match remainder[0] {
-                b'i' => Self::parse_int(remainder),
-                b'0'..b'9' => Self::parse_string(remainder),
-                b'l' => Self::parse_list(remainder),
-                _ => Err(format!("Invalid first byte: {}", remainder[0]))
-            };
-            let (elem, _remainder) = elem_wrapper?;
+            let (elem, _remainder) = Self::parse(remainder)?;
             remainder = _remainder;
-            value.push(elem);
+            list.push(elem);
         }
-        Ok((Bencode::List(value), &remainder[1..]))
+
+        Ok((Bencode::List(list), &remainder[1..]))
     }
+
+    fn parse_dict(bytes: &[u8]) -> Result<(Self, &[u8]), String> {
+        let mut dict = BTreeMap::<Vec<u8>, Bencode>::new();
+        let mut remainder = &bytes[1..];
+
+        while remainder.len() > 0 {
+            if remainder[0] == b'e' {
+                break;
+            }
+            let (key, _remainder) = Self::parse_string_raw(remainder)?;
+            let (value, _remainder) = Self::parse(_remainder)?;
+            remainder = _remainder;
+            dict.insert(key, value);
+        }
+
+        Ok((Bencode::Dict(dict), &remainder[1..]))
+    }
+
+    fn parse(bytes: &[u8]) -> Result<(Self, &[u8]), String> {
+        match bytes[0] {
+            b'i' => Self::parse_int(bytes),
+            b'0'..b'9' => Self::parse_string(bytes),
+            b'l' => Self::parse_list(bytes),
+            b'd' => Self::parse_dict(bytes),
+            _ => Err(format!("Invalid first byte: {}", bytes[0])),
+        }
+    }
+
 }
 
 #[cfg(test)]
@@ -107,7 +132,24 @@ mod tests {
     #[test]
     fn test_list_basic() {
         let (res, remainder) = Bencode::parse_list(b"l4:spami42eeLOL").unwrap();
-        assert_eq!(res, Bencode::List(vec!(Bencode::String(Vec::from(b"spam")), Bencode::Integer(42))));
+        assert_eq!(
+            res,
+            Bencode::List(vec!(
+                Bencode::String(Vec::from(b"spam")),
+                Bencode::Integer(42)
+            ))
+        );
+        assert_eq!(remainder, b"LOL");
+    }
+
+    #[test]
+    fn test_dict_basic() {
+        let (res, remainder) = Bencode::parse_dict(b"d3:bar4:spam3:fooi42eeLOL").unwrap();
+        let expected = BTreeMap::from([
+            (Vec::from(b"bar"), Bencode::String(Vec::from(b"spam"))),
+            (Vec::from(b"foo"), Bencode::Integer(42)),
+        ]);
+        assert_eq!(res, Bencode::Dict(expected));
         assert_eq!(remainder, b"LOL");
     }
 }
